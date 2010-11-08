50d5bb1a4fd261b778db68e27535a4f944582767
Pavel Panchekha <pavpanchekha@gmail.com>
2010-11-07 21:04:38 -0500

    Configurable temporary directory

--- moviedir/mplayer.py
+++ moviedir/mplayer.py
@@ -4,28 +4,29 @@ import os
 import cPickle as pickle
 
 MPLAYER="/usr/bin/mplayer"
-FIFO="/tmp/mplayer.fifo"
-STATE="/tmp/mplayer.pickle.2"
 
 class MPlayer(object):
-    def __init__(self, dir):
+    def __init__(self, dir, tmpdir):
+        self.FIFO = os.path.join(tmpdir, "mplayer.fifo")
+        self.FIFO = os.path.join(tmpdir, "mplayer.pickle")
+        
         self.dir = dir
-        if os.path.exists(STATE):
-            self.state = pickle.load(open(STATE, "rb"))
+        if os.path.exists(self.STATE):
+            self.state = pickle.load(open(self.STATE, "rb"))
         else:
             self.state = {"playing": False, "movie": None}
             self.sync_state()
 
     def sync_state(self):
-        pickle.dump(self.state, open(STATE, "wb"))
+        pickle.dump(self.state, open(self.STATE, "wb"))
 
     def make_fifo(self):
-        if os.path.exists(FIFO):
+        if os.path.exists(self.FIFO):
             self.rm_fifo()
-        os.mkfifo(FIFO)
+        os.mkfifo(self.FIFO)
 
     def rm_fifo(self):
-        os.unlink(FIFO)
+        os.unlink(self.FIFO)
     
     def start(self, f):
         if self.state["movie"] is not None:
@@ -33,7 +34,7 @@ class MPlayer(object):
         self.state["movie"] = f
         self.state["playing"] = True
         self.make_fifo()
-        subprocess.Popen([MPLAYER, "-fs", "-zoom", "-display", ":0.0", "-idle", "-slave", "-input", "file="+os.path.abspath(FIFO), os.path.abspath(os.path.join(self.dir, f))], stderr=open("/dev/null"))
+        subprocess.Popen([MPLAYER, "-fs", "-zoom", "-display", ":0.0", "-idle", "-slave", "-input", "file="+os.path.abspath(self.FIFO), os.path.abspath(os.path.join(self.dir, f))], stderr=open("/dev/null"))
         self.sync_state()
 
     def send_command(self, cmd):
